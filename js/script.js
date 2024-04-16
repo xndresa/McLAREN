07637afda41e79db52c815ff28eb68a020899470
xndresa <andresaalves9@gmail.com>
2024-04-16 20:20:02 -0300

    commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// toggle menu button
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.nav');
+    menu.classList.toggle('active');
+    nav.classList.toggle('active');
+}
