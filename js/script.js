1c5e2a1cd0955097d85b6177a2b95055e9065414
xndresa <andresaalves9@gmail.com>
2024-06-03 13:31:19 -0300

    alterações

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ function toggleMenu() {
     menu.classList.toggle('active');
     nav.classList.toggle('active');
 }
+
+// background 
+function changeVideo(name){
+    const bgVideoList = document.querySelectorAll('.bg-video');
+    const trailers = document.querySelectorAll('.trailer');
+    const models = document.querySelectorAll('.model');
+
+    bgVideoList.forEach(video => {
+        video.classList.remove('active');
+        if(video.classList.contains(name)) {
+          video.classList.add('active');
+        }
+    })
+}
